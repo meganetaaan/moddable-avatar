ee8b0da7b7c1018bd70088eed383fb8f2c380013
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2019-02-25 00:25:06 +0900

    Add Piu type decleration

---
+++ types/piu.d.ts
@@ -0,0 +1,18 @@
+declare namespace piu {
+  class Behavior {}
+  class Content {}
+  class Skin {}
+  class Style {}
+  class Texture {}
+  class Transition {}
+  class Container extends Content {}
+  class Label extends Content {}
+  class Port extends Content {}
+  class Text extends Content {}
+  class Application extends Container {}
+  class Column extends Container {}
+  class Layout extends Container {}
+  class Die extends Layout {}
+  class Row extends Container {}
+  class Scroller extends Container {}
+}
