6d2368d77721ee7a0c9d8de9bf069227108e9c21
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2020-01-18 13:48:11 +0900

    Add marquee label

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Avatar from 'avatar'
-import { BalloonLabel } from 'balloon'
 import { Application, Skin } from 'piu/MC'
+import MarqueeLabel from 'marquee-label'
+
+const SPEECH_STR =
+  'わが輩は猫である。名前はまだ無い。　どこで生れたかとんと見当けんとうがつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。'
 
 const fluid = {
   top: 0,
@@ -17,12 +20,22 @@ const ap = new Application(null, {
       width: 320,
       height: 240,
     }),
+    /*
     new BalloonLabel({
       bottom: 30,
       right: 40,
       width: 80,
       height: 30,
       string: 'Hello',
+    }),
+    */
+    new MarqueeLabel({
+      state: 0,
+      bottom: 10,
+      right: 10,
+      width: 180,
+      height: 40,
+      string: SPEECH_STR,
     }),
   ],
 })
